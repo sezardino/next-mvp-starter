e48ae9f2bb4cbd4948dc9aab14ae6d2e99d5c28d
Edward <e.arechwa@gmail.com>
2024-10-02 14:48:29 +0200

    ft: add nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "@/styles/index.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { PropsWithChildren } from "react";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Next mvp starter",
+  description: "Starter for create mvp with nextjs",
+};
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
